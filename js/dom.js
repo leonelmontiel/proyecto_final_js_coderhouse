cc6ae5648623e29e5f885ba949951052cc62bcc0
leonelmontiel <leonel.montiel@outlook.com>
2022-04-14 16:55:53 -0300

    probando subida

---
+++ js/dom.js
@@ -0,0 +1,27 @@
+let feriados = document.getElementById("feriados");
+
+let identificadorIntervaloDeTiempo;
+
+function repetirCadaSegundo() {
+  identificadorIntervaloDeTiempo = setInterval(horarios, 3000);
+}
+let horarios1 = feriados.innerText;
+let horarios2 = "Sábado de 10h a 16h";
+let horarios3 = "FERIADOS CERRADO";
+
+function horarios() {
+    switch (feriados.innerText) {
+        case horarios1:
+            feriados.innerText = horarios2;
+            break;
+        case horarios2:
+            feriados.innerText = horarios3;
+            break;
+        case horarios3:
+            feriados.innerText = horarios1;
+            break;
+    }
+}
+
+repetirCadaSegundo();
+
