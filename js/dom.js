035a11b4c05f4a54c6fc315e658c1271c2fb8ba7
Leonel Montiel <leonel.montiel@outlook.com>
2022-04-15 19:48:24 -0300

    agrego clase Formulario y almaceno los forms en una base de datos

--- js/dom.js
+++ js/dom.js
@@ -31,20 +31,40 @@ repetirCadaSegundo();
 
 // CÓDIGO PARA DESAFÍO: INCORPORAR EVENTOS
 
-let form = document.getElementById("form");
-form.addEventListener("submit", validarFormulario);
+class Formulario {
+    constructor(nombrePersona, asunto, email, mensaje) {
+        this.nombre = nombrePersona;
+        this.asunto = asunto;
+        this.email = email;
+        this.mensaje = mensaje;
+    }
+}
+
+let baseDeDatosForm = []; //lista de formularios ingresados
+
+function almacenarForm(nombre, correo, asunto, mensaje) {
+    const newForm = new Formulario(nombre, asunto, correo, mensaje);
+    baseDeDatosForm.push(newForm);
+}
+
+
+let formulario = document.getElementById("form");
+formulario.addEventListener("submit", validarFormulario);
 
 function validarFormulario(e) {
     e.preventDefault();
-    formulario = e.target;
-    nombre = formulario.children[0].children[0].value;
-    correo = formulario.children[1].children[0].value;
-    asunto = formulario.children[2].children[0].value;
-    mensaje = formulario.children[3].children[1].value;
+    let form = e.target;
+    let nombre = form.children[0].children[0].value;
+    let correo = form.children[1].children[0].value;
+    let asunto = form.children[2].children[0].value;
+    let mensaje = form.children[3].children[1].value;
+    //Cada vez que se envíe un formulario se va a almacenar en la base de datos
+    almacenarForm(nombre, correo, asunto, mensaje);
     console.log(`Datos del formulario ingresado:
     Nombre: ${nombre}
     Correo: ${correo}
     Asunto: ${asunto}
     Mensaje: ${mensaje}`);
+    console.log(`CANTIDAD DE FORMULARIOS EN BASE DE DATOS: ${baseDeDatosForm.length}`);
     alert(`Muchas gracias ${nombre}, en breve nos contactaremos con vos!`)
 }
